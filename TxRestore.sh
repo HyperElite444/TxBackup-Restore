a4a22030cc87b004d2a485970f1e104e44b82951
Mʀ.Tʜᴇɴᴜx <88967143+HyperElite444@users.noreply.github.com>
2022-03-07 13:36:12 +0530

    Create TxRestore.sh

---
+++ TxRestore.sh
@@ -0,0 +1,21 @@
+#TOOL BY MR.THENUX
+
+clear
+
+termux-setup-storage
+
+pkg install figlet -y
+pkg install lolcat -y
+
+clear
+
+echo
+figlet -f small Tx Termux Restore | lolcat
+
+cd /data/data/com.termux/files
+tar -xvzf /sdcard/Termux_Backup/termux.tgz
+
+figlet -f small Successfully Restored... | lolcat
+echo
+
+echo && printf "TOOL BY MR.THENUX" | lolcat
